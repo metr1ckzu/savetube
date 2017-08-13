4d54c45979fb11350ff8c1b3be939e86eeea55e0
Smolyaeva Ektaterina <SmolyaevaEktaterina@gmail.com>
2017-08-13 21:21:33 +0600

    ready with buds

--- downloader/models.py
+++ downloader/models.py
@@ -1,1 +1,8 @@
 from django.db import models
+
+
+class Saver(models.Model):
+    source_link = models.CharField(max_length=500)
+
+    def __unicode__(self):
+        return self.source_link
--- downloader/views.py
+++ downloader/views.py
@@ -1,9 +1,24 @@
+from django.http import HttpResponse
+from django.shortcuts import render_to_response
 from django.views.generic import TemplateView
+from downloader.models import Saver
+from youtube_dl import YoutubeDL
 
 
 class IndexView(TemplateView):
     template_name = 'downloader/index.html'
 
+    def redirect(request):
+        source_link = Saver()
+        source_link.source_url = request.POST['source_url']
+        source_link.save()
+        return render_to_response('downloader/saver_page.html')
+
+    def saver_link(request):
+        source_link = Saver()
+        dest_link = YoutubeDL().extract_info(source_link, download=False)
+        return dest_link
+        #return HttpResponse({ % dest_link %})
 
 class SaveView(TemplateView):
     template_name = 'downloader/saver_page.html'
