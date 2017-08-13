0c36be2d02f923a4b1eda5bd648a42580552f77c
Smolyaeva Ektaterina <SmolyaevaEktaterina@gmail.com>
2017-08-13 13:15:49 +0600

    index page

--- yousave/urls.py
+++ yousave/urls.py
@@ -1,8 +1,9 @@
-from django.conf.urls import include, url
+from django.conf.urls import url
 from django.contrib import admin
 from downloader import views
 
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^$', views.index, name='index'),
+    url(r'^$', views.IndexView.as_view(), name='index'),
 ]
