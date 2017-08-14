4567ccff46539e04203009545bceb78b8fd095d6
metr1ckzu <metr1ckzu@gmail.com>
2017-08-14 23:07:17 +0600

    views and template change

--- downloader/views.py
+++ downloader/views.py
@@ -13,6 +13,8 @@ def submit(request):
         result_url = requests.get('http://www.youtubeinmp3.com/widget/button/?id={}'.format(source))
         result_url.encoding = 'utf-8'
         result_url_parsed = BeautifulSoup(result_url.text)
+        old_download_link = result_url_parsed.html.body.a['href']
+        result_url_parsed.html.body.a['href'] = 'http://www.youtubeinmp3.com/' + old_download_link
         result_url_body = result_url_parsed.html.body
         return render(request, result_template_name, {'result_url_body': result_url_body})
 
